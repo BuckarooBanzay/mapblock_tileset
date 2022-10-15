01ae0b907adf2fe6af63e0c9db9b8b9f3e816739
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-10-15 12:26:48 +0200

    sort by category

--- api.lua
+++ api.lua
@@ -1,9 +1,23 @@
 
 local tilesets = {}
+local categories = {}
 
 function mapblock_tileset.register_tileset(name, tileset_def)
     tileset_def.name = name
+    tileset_def.category = tileset_def.category or "default"
     tilesets[name] = tileset_def
+
+    for _, c in ipairs(categories) do
+        if c == tileset_def.category then
+            -- category already exists
+            return
+        end
+    end
+    table.insert(categories, tileset_def.category)
+end
+
+function mapblock_tileset.get_categories()
+    return categories
 end
 
 function mapblock_tileset.get_tileset(name)
@@ -12,4 +26,15 @@ end
 
 function mapblock_tileset.get_tilesets()
     return tilesets
+end
+
+function mapblock_tileset.get_tileset_list_by_category(category)
+    local list = {}
+    for _, tileset_def in pairs(tilesets) do
+        if tileset_def.category == category then
+            table.insert(list, tileset_def)
+        end
+    end
+    table.sort(list, function(a,b) return a.name < b.name end)
+    return list
 end
--- tools.lua
+++ tools.lua
@@ -1,38 +1,51 @@
 
 local formname = "mapblock_tileset_placer_configure"
 
-local tileset_list = {}
-minetest.register_on_mods_loaded(function()
-    for tilesetname in pairs(mapblock_tileset.get_tilesets()) do
-        table.insert(tileset_list, tilesetname)
-    end
-    table.sort(tileset_list)
-end)
-
 local function get_formspec(itemstack)
     local meta = itemstack:get_meta()
+    local selected_category = meta:get_string("category") or "default"
+
+    local tileset_list = mapblock_tileset.get_tileset_list_by_category(selected_category)
+
     local selected_tilesetname = meta:get_string("tilesetname")
     if not selected_tilesetname or selected_tilesetname == "" then
         selected_tilesetname = tileset_list[1]
     end
 
-    local selected = 1
-    local list = ""
+    local selected_tileset = 1
+    local textlist = ""
 
-    for i, tilesetname in ipairs(tileset_list) do
+    for i, tileset_def in ipairs(tileset_list) do
+        local tilesetname = tileset_def.name
         if selected_tilesetname == tilesetname then
-            selected = i
+            selected_tileset = i
         end
 
-        list = list .. tilesetname
+        textlist = textlist .. tilesetname
         if i < #tileset_list then
-            list = list .. ","
+            textlist = textlist .. ","
         end
     end
 
-    return "size[8,6;]" ..
-        "textlist[0,0.1;8,5;tilesetname;" .. list .. ";" .. selected .. "]" ..
-        "button_exit[0.1,5.5;8,1;back;Back]"
+    local categories = mapblock_tileset.get_categories()
+    local selected_category_index = 1
+    local cat_list = ""
+
+    for i, category in ipairs(categories) do
+        if category == selected_category then
+            selected_category_index = i
+        end
+
+        cat_list = cat_list .. category
+        if i < #categories then
+            cat_list = cat_list .. ","
+        end
+    end
+
+    return "size[8,7;]" ..
+        "dropdown[0,0.1;8;category;" .. cat_list .. ";" .. selected_category_index .. "]" ..
+        "textlist[0,1.1;8,5;tilesetname;" .. textlist .. ";" .. selected_tileset .. "]" ..
+        "button_exit[0.1,6.5;8,1;back;Back]"
 end
 
 minetest.register_on_player_receive_fields(function(player, f, fields)
@@ -42,18 +55,35 @@ minetest.register_on_player_receive_fields(function(player, f, fields)
     if formname ~= f then
         return
     end
+    if fields.quit then
+        return
+    end
 
     if fields.tilesetname then
         local parts = fields.tilesetname:split(":")
         if parts[1] == "CHG" then
-            local selected = tonumber(parts[2])
-            local tilesetname = tileset_list[selected]
             local itemstack = player:get_wielded_item()
             local meta = itemstack:get_meta()
-            meta:set_string("tilesetname", tilesetname)
-            meta:set_string("description", "Selected tileset: '" .. tilesetname .. "'")
+
+            local selected = tonumber(parts[2])
+            local selected_category = meta:get_string("category") or "default"
+            local tileset_list = mapblock_tileset.get_tileset_list_by_category(selected_category)
+            local tileset_def = tileset_list[selected]
+            if not tileset_def then
+                return
+            end
+
+            meta:set_string("tilesetname", tileset_def.name)
+            meta:set_string("description", "Selected tileset: '" .. tileset_def.name .. "'")
             player:set_wielded_item(itemstack)
         end
+
+    elseif fields.category then
+        local itemstack = player:get_wielded_item()
+        local meta = itemstack:get_meta()
+        meta:set_string("category", fields.category)
+        player:set_wielded_item(itemstack)
+        minetest.show_formspec(player:get_player_name(), formname, get_formspec(itemstack))
     end
 end)
 
